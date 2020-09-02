36f2f6520fb55d3059d7b0443b330e4992be9d1c
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-09-02 21:12:00 +0200

    Add Array for images

--- script/script.js
+++ script/script.js
@@ -1,1 +1,39 @@
-console.log(8);
+// let picArray = ['../img/rock.svg'];
+
+// let img = document;
+// document.querySelector('#pictest').src = picArray[0];
+
+let imgArray = ['../img/rock.svg', '../img/paper.svg', '../img/scissors.svg'];
+
+function loadImagesChoice(){
+    document.querySelector('#imgChoiceRock').src = imgArray[0];
+    document.querySelector('#imgChoicePaper').src = imgArray[1];
+    document.querySelector('#imgChoiceScissors').src = imgArray[2];
+}
+
+function removeEventListenersFromChoice(){
+
+    document.querySelector('#imgChoiceRock').removeEventListener('click', playAsRock);
+
+}
+
+
+function playAsRock(){
+    // show image you choose
+    let imgEl = document.createElement("img");
+    imgEl.src = imgArray[0];
+    document.querySelector("#youPic").appendChild(imgEl);
+    
+    removeEventListenersFromChoice();
+    
+    
+}
+
+
+
+
+// EventListeners
+window.addEventListener('load', loadImagesChoice);
+document.querySelector('#imgChoiceRock').addEventListener('click', playAsRock);
+// document.querySelector('#imgChoicePaper').addEventListener('click', test);
+// document.querySelector('#imgChoiceScissors').addEventListener('click', test);
